b37713e77e3432f34947e69c3cdf14fc2229c62b
mpuebla <mpuebla@focasoftware.com>
2020-02-13 20:48:12 -0300

    Add interfaz de resultados de busqueda

---
+++ src/app/utils/ISearchResult.ts
@@ -0,0 +1,25 @@
+export interface ISearchResult {
+    author: IAuthor;
+    categories: string[];
+    items: IItem[];
+}
+
+export interface IAuthor {
+    name: string;
+    lastname: string;
+}
+
+export interface IPrice {
+    currency: string;
+    amount: number;
+    decimals: number;
+}
+
+export interface IItem {
+    id: string;
+    title: string;
+    price: IPrice;
+    picture: string;
+    condition: string;
+    free_shipping: boolean;
+}
