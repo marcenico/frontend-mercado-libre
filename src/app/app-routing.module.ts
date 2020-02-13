b6109ad75c2f204bbb9dfa2bd1cccb5d3eef5040
Marcelo Puebla <mpuebla@focasoftware.com>
2020-02-13 12:40:37 -0300

    Fix Nombre de carpeta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HeaderComponent } from './components/search-box/header.component';
+import { HeaderComponent } from './components/header/header.component';
 
 const routes: Routes = [
   { path: '', component: HeaderComponent }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/search-box/header.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
